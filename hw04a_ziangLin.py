de958756a7e9fb8e8e164c409c87e6488750b303
Ziang Lin <zlin17@stevens.edu>
2019-03-04 15:08:21 -0500

    HW05a, updated hw04's script to meet the requirements of hw05a

--- hw04a_ziangLin.py
+++ hw04a_ziangLin.py
@@ -8,8 +8,27 @@ import requests
 import json
 
 def get_github_repo_commit(user_id):
-    repo_url = "https://api.github.com/users/%s/repos" %(user_id)
     #print(repo_url)
+    #print(type(repo_list))
+    res = []
+    repo_list = send_request(user_id)
+    if (type(repo_list) == dict and repo_list['message']) or repo_list == []:
+        if repo_list['message'] == "API rate limit exceeded for 98.109.30.29. (But here's the good news: Authenticated " \
+                                   "requests get a higher rate limit. Check out the documentation for more details.)":
+            #print("API rate limit exceeded.")
+            return -1
+        #print('The user ID not found')
+        return 0
+    for repo in repo_list:
+        repo_name = repo['name']
+        #print(repo_name)
+        commit_list = get_repo_commit(user_id, repo_name)
+        print("Repo: %s Number of commits: %s" %(repo_name, len(commit_list)))
+        res.append("Repo: %s Number of commits: %s" %(repo_name, len(commit_list)))
+    return res
+
+def send_request(user_id):
+    repo_url = "https://api.github.com/users/%s/repos" % (user_id)
     try:
         json_repo = requests.get(repo_url)
     except requests.HTTPError:
@@ -17,29 +36,14 @@ def get_github_repo_commit(user_id):
         return -1
     #print(json_repo.text)
     repo_list = json.loads(json_repo.text)
-    #print(type(repo_list))
-    res = []
+    return repo_list
 
-    if (len(repo_list) == 2 and repo_list['message']) or repo_list == []:
-        if repo_list['message'] == "API rate limit exceeded for 98.109.30.29. (But here's the good news: Authenticated " \
-                                   "requests get a higher rate limit. Check out the documentation for more details.)":
-            print("API rate limit exceeded.")
-            return -1
-        print('The user ID not found')
-        return 0
-    for repo in repo_list:
-        repo_name = repo['name']
-        #print(repo_name)
-        commiit_url = "https://api.github.com/repos/%s/%s/commits" %(user_id, repo_name)
-        try:
-            json_commit = requests.get(commiit_url)
-        except requests.HTTPError:
-            print('Invalid request')
-            return -1
-        commit_list = json.loads(json_commit.text)
-        print("Repo: %s Number of commits: %s" %(repo_name, len(commit_list)))
-        res.append("Repo: %s Number of commits: %s" %(repo_name, len(commit_list)))
-    return res
-
-
-#get_github_repo_commit("daseda")
+def get_repo_commit(user_id, repo_name):
+    commiit_url = "https://api.github.com/repos/%s/%s/commits" % (user_id, repo_name)
+    try:
+        json_commit = requests.get(commiit_url)
+    except requests.HTTPError:
+        print('Invalid request')
+        return -1
+    return json.loads(json_commit.text)
+get_github_repo_commit("richkempinski")
